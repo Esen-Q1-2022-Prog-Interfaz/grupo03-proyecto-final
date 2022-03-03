d6c88337ba8f9c791071870baa75840a5153a594
Marcela Gallardo <97997898+Marce2022@users.noreply.github.com>
2022-03-02 22:56:22 -0600

    archivos iniciales

---
+++ scripts.sql
@@ -0,0 +1,15 @@
+/*
+import pymysql
+from pymysql import cursors
+
+def connection_db():
+    connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        passwd="*CONTRA*",
+        port=3306,
+        database="*DB*",
+        cursorclass=cursors.DictCursor        
+    )
+    return connection
+*/ 
---
+++ test.py
@@ -0,0 +1,9 @@
+def sum(a, b):
+    return a + b
+# Hacer los las funciones a testear aqui
+
+def test():
+    # agregar los resultados aqui
+    assert sum(1, 2) == 3
+
+# python -m pytest -v test.py
