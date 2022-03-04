f76b70c2f32b0f9f74069bc98d6739974b3705dd
Cernacls <20200053@esen.edu.sv>
2022-03-03 18:57:45 -0600

    cloning files de la version anterior en main

---
+++ models/actividades.py
@@ -0,0 +1,6 @@
+class Actividad:
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return f""
---
+++ models/inscripciones.py
@@ -0,0 +1,6 @@
+class Inscripcion:
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return f""
