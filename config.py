a7852dfe9c098932601e0bb551c9dafc1ab58a0d
Cernacls <20200053@esen.edu.sv>
2022-03-06 14:50:33 -0600

    Comments Cerna

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 DATA_BASE_NAME = 'pruebaxd'
 USER = "root"
-PASSWORD = "rootroot"
+PASSWORD = "Ecerna2310"
 
 class BaseConfig(object):
-    SQLALCHEMY_DATABASE_URI = f"mysql://{USER}:{PASSWORD}@localhost:3306/{DATA_BASE_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"mysql://root:Ecerna2310@localhost:3306/pruebaxd"
     SQLALCHEMY_POOL_SIZE = 50
     SQLALCHEMY_POOL_TIMEOUT = 300
     SQLALCHEMY_TRACK_MODIFICATIONS = False
